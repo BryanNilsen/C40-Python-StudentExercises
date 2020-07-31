2114123d30d5e571ee5371ae0170117461bb146e
BryanNilsen <bryannilsen@gmail.com>
2020-07-31 15:58:58 -0500

    completed part 4

--- queries.sql
+++ queries.sql
@@ -13,3 +13,12 @@ select s.student_id,
 from Students s
 join Cohorts c on s.cohort_id = c.cohort_id
 order by s.cohort_id
+
+select i.instructor_id,
+    i.first_name,
+    i.last_name,
+    i.slackhandle,
+    i.cohort_id,
+    c.name
+from Instructors i
+join Cohorts c on i.cohort_id = c.cohort_id
